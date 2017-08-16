0280f87aa7178d54a03d9d597958608fef0ffa0b
Johnathan <jaketheace1@gmail.com>
2017-08-16 12:02:10 -0500

    Add some comments to WMW_Scraper.py

--- WMW_Scraper.py
+++ WMW_Scraper.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 
+# If you want to change the chapters that get downloaded, just change startChap and endChap.
+# If you want to change to a different story, just change the URL 
+#   in "chapterData = requests.get("...")" to whatever the book uses.
+
+
 from optparse import OptionParser
 from bs4 import BeautifulSoup
 from time import sleep
 import requests
 
+startChap = 551
+endChap = 850
 
-for i in range(551,851):
+for i in range(startChap,endChap + 1):
     sleep(1)
     chapterData = requests.get("http://www.wuxiaworld.com/wmw-index/wmw-chapter-" + str(i))
     chapter = chapterData.text
